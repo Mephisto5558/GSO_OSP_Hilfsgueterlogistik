9e288a9956e38265308b3dc2b8e4fad3b140730f
Mephisto5558 <87113906+Mephisto5558@users.noreply.github.com>
2026-02-03 13:52:41 +0100

    add deploy ci and scripts

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,9 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default {
   plugins: [tsconfigPaths()],
+  root: './src/frontend/pages',
   build: {
-    outDir: '../../dist/frontend',
+    outDir: '../../../dist/frontend',
     emptyOutDir: true
   }
 };
