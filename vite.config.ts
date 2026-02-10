4f88f491bbca88b52d3f3c855e321f6db62f84eb
Mephisto5558 <87113906+Mephisto5558@users.noreply.github.com>
2026-02-10 09:16:37 +0100

    improve vite config: minification & sourceMaps

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,20 @@
+import { resolve } from 'node:path';
 import tsconfigPaths from 'vite-tsconfig-paths';
+
+import type { UserConfig } from 'vite';
 
 export default {
   plugins: [tsconfigPaths()],
-  root: './src/frontend/pages',
+  root: './src/frontend/',
   build: {
-    outDir: '../../../dist/frontend',
-    emptyOutDir: true
+    outDir: '../../dist/frontend',
+    sourcemap: true,
+    emptyOutDir: true,
+    minify: 'esbuild',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'src/frontend/pages/index.html')
+      }
+    }
   }
-};
+} as UserConfig;
