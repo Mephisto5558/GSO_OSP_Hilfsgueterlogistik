641a77e9b9ad42f58e735cdb7a18417915abd25a
Mephisto5558 <87113906+Mephisto5558@users.noreply.github.com>
2026-02-10 13:01:53 +0100

    fix watch logic

--- vite.config.ts
+++ vite.config.ts
@@ -1,36 +1,43 @@
 import { readdirSync } from 'node:fs';
 import { extname, join, relative, resolve } from 'node:path';
+import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-import type { UserConfig } from 'vite';
-
 const
-  root = resolve(__dirname, 'src/frontend'),
-  pagesDir = resolve(root, 'pages');
+  rootForBuild = resolve(import.meta.dirname, 'src/frontend'),
+  pagesDir = resolve(rootForBuild, 'pages'),
 
-function findHtmlEntryPoints(dir: string): string[] {
-  return readdirSync(dir, { withFileTypes: true }).flatMap(entry => {
-    const fullPath = join(dir, entry.name);
-    if (entry.isDirectory()) return findHtmlEntryPoints(fullPath);
-    if (entry.isFile() && extname(entry.name) == '.html') return [fullPath];
+  findHtmlEntryPoints = (dir: string): string[] => readdirSync(dir, { withFileTypes: true, recursive: true })
+    .filter(e => e.isFile() && e.name.endsWith('.html'))
+    .map(e => join(e.parentPath, e.name)),
 
-    return [];
-  });
-}
+  input = Object.fromEntries(findHtmlEntryPoints(pagesDir).map(file => [
+    relative(pagesDir, file.slice(0, -extname(file).length)).replaceAll('\\', '/'),
+    file
+  ]));
 
-const input = Object.fromEntries(findHtmlEntryPoints(pagesDir).map(file => [
-  relative(root, file.slice(0, file.length - '.html'.length)).replaceAll('\\', '/'),
-  file
-]));
-
-export default {
+export default defineConfig(({ command }) => ({
+  root: command === 'serve' ? pagesDir : rootForBuild,
+  publicDir: resolve(rootForBuild, 'assets'),
   plugins: [tsconfigPaths()],
-  root: './src/frontend/',
+  resolve: {
+    alias: {
+      '/styles': resolve(rootForBuild, 'styles'),
+      '/scripts': resolve(rootForBuild, 'scripts')
+    }
+  },
   build: {
-    outDir: '../../dist/frontend',
+    outDir: '../../../dist/frontend',
     sourcemap: true,
     emptyOutDir: true,
     minify: 'esbuild',
-    rollupOptions: { input }
+    rollupOptions: {
+      input,
+      output: {
+        entryFileNames: 'assets/[name].entry-[hash].js',
+        chunkFileNames: 'assets/[name].chunk-[hash].js',
+        assetFileNames: 'assets/[name].asset-[hash].[ext]'
+      }
+    }
   }
-} as UserConfig;
+}));
