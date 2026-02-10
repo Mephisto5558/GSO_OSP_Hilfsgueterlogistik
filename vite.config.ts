08c427a1d3941ffb9e396c6b39ca415520be191e
Mephisto5558 <87113906+Mephisto5558@users.noreply.github.com>
2026-02-10 09:56:41 +0100

    dynamically generate entry points

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,27 @@
-import { resolve } from 'node:path';
+import { readdirSync } from 'node:fs';
+import { extname, join, relative, resolve } from 'node:path';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 import type { UserConfig } from 'vite';
+
+const
+  root = resolve(__dirname, 'src/frontend'),
+  pagesDir = resolve(root, 'pages');
+
+function findHtmlEntryPoints(dir: string): string[] {
+  return readdirSync(dir, { withFileTypes: true }).flatMap(entry => {
+    const fullPath = join(dir, entry.name);
+    if (entry.isDirectory()) return findHtmlEntryPoints(fullPath);
+    if (entry.isFile() && extname(entry.name) == '.html') return [fullPath];
+
+    return [];
+  });
+}
+
+const input = Object.fromEntries(findHtmlEntryPoints(pagesDir).map(file => [
+  relative(root, file.slice(0, file.length - '.html'.length)).replaceAll('\\', '/'),
+  file
+]));
 
 export default {
   plugins: [tsconfigPaths()],
@@ -11,10 +31,6 @@ export default {
     sourcemap: true,
     emptyOutDir: true,
     minify: 'esbuild',
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'src/frontend/pages/index.html')
-      }
-    }
+    rollupOptions: { input }
   }
 } as UserConfig;
