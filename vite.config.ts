2450d419191f0ee2269b6d79ae867627f74fc6d0
lehne <lehne@lemm.de>
2026-02-17 09:09:40 +0100

    Revert "Merge branch 'frontend' of https://github.com/Mephisto5558/GSO_OSP_Hilfsgueterlogistik into frontend"
    This reverts commit d92620864af64cc2eda2e41471e3793d9b9dce9c, reversing
    changes made to 657871121c64ae2b32add6adb0c42f58d2b38180.

--- vite.config.ts
+++ vite.config.ts
@@ -1,41 +1,5 @@
 import { readdirSync } from 'node:fs';
 import { extname, join, relative, resolve } from 'node:path';
-import tsconfigPaths from 'vite-tsconfig-paths';
-
-import type { UserConfig } from 'vite';
-
-const
-  root = resolve(__dirname, 'src/frontend'),
-  pagesDir = resolve(root, 'pages');
-
-function findHtmlEntryPoints(dir: string): string[] {
-  return readdirSync(dir, { withFileTypes: true }).flatMap(entry => {
-    const fullPath = join(dir, entry.name);
-    if (entry.isDirectory()) return findHtmlEntryPoints(fullPath);
-    if (entry.isFile() && extname(entry.name) == '.html') return [fullPath];
-
-    return [];
-  });
-}
-
-const input = Object.fromEntries(findHtmlEntryPoints(pagesDir).map(file => [
-  relative(root, file.slice(0, file.length - '.html'.length)).replaceAll('\\', '/'),
-  file
-]));
-
-export default {
-  plugins: [tsconfigPaths()],
-  root: './src/frontend/',
-  build: {
-    outDir: '../../dist/frontend',
-    sourcemap: true,
-    emptyOutDir: true,
-    minify: 'esbuild',
-    rollupOptions: { input }
-  }
-} as UserConfig;
-import { readdirSync } from 'node:fs';
-import { extname, join, relative, resolve } from 'node:path';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
