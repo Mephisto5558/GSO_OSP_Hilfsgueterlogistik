7be2e495b8173d44c3b1e87db0ab31bbc3e1bedf
Mephisto5558 <87113906+Mephisto5558@users.noreply.github.com>
2026-02-17 14:14:52 +0100

    Add testing logic (#9)
    * implement basic folder structure with tsconfigs
    
    * replace eslint.yml with ci.yml and vite build steps.
    
    * add package.json
    
    * add deploy ci and scripts
    
    * push to release branch instead of main
    
    * remove test log
    
    * remove temporary content
    
    * improve vite config: minification & sourceMaps
    
    * re-add dist
    
    * improve tsconfigs
    
    * fix watch logic
    
    * add testing config
    
    * move `src/tests` to `/tests`
    
    * add testing config
    
    * fix setup.yml
    
    * fix environment resolution and tsconfig splitting
    
    * update vitest config
    
    * fix action.yml path
    
    * add json-summary reporter
    
    * install package-lock.json
    
    * add test command to PR template

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
+/* eslint-disable-next-line @typescript-eslint/triple-slash-reference -- https://vitest.dev/guide/#configuring-vitest */
+/// <reference types="vitest/config" />
+
 import { readdirSync } from 'node:fs';
 import { extname, join, relative, resolve } from 'node:path';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 const
+  REQUIRED_COVERAGE = 80,
   rootForBuild = resolve(import.meta.dirname, 'src/frontend'),
   pagesDir = resolve(rootForBuild, 'pages'),
 
@@ -14,17 +18,48 @@ const
   input = Object.fromEntries(findHtmlEntryPoints(pagesDir).map(file => [
     relative(pagesDir, file.slice(0, -extname(file).length)).replaceAll('\\', '/'),
     file
-  ]));
+  ])),
+
+  alias = {
+    '@': resolve(import.meta.dirname, 'src'),
+    '/styles': resolve(rootForBuild, 'styles'),
+    '/scripts': resolve(rootForBuild, 'scripts')
+  };
 
 export default defineConfig(({ command }) => ({
   root: command === 'serve' ? pagesDir : rootForBuild,
   publicDir: resolve(rootForBuild, 'assets'),
   plugins: [tsconfigPaths()],
-  resolve: {
-    alias: {
-      '/styles': resolve(rootForBuild, 'styles'),
-      '/scripts': resolve(rootForBuild, 'scripts')
-    }
+  resolve: { alias },
+  test: {
+    root: resolve(import.meta.dirname),
+    css: false,
+    coverage: {
+      provider: 'istanbul',
+      reporter: ['text', 'json', 'json-summary', 'html'],
+      thresholds: {
+        statements: REQUIRED_COVERAGE,
+        branches: REQUIRED_COVERAGE,
+        functions: REQUIRED_COVERAGE,
+        lines: REQUIRED_COVERAGE
+      }
+    },
+    projects: [
+      {
+        resolve: { alias },
+        test: {
+          environment: 'jsdom',
+          include: ['**/tests/frontend/**/*.test.{js,mjs,cjs,ts,mts,cts,jsx,tsx}']
+        }
+      },
+      {
+        resolve: { alias },
+        test: {
+          environment: 'node',
+          include: ['**/tests/backend/**/*.test.{js,mjs,cjs,ts,mts,cts,jsx,tsx}']
+        }
+      }
+    ]
   },
   build: {
     outDir: resolve(import.meta.dirname, 'dist/frontend'),
