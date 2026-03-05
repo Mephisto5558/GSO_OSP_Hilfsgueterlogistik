ff0747376cc238e9dd316b4e8468be70017dab8e
Mephisto5558 <87113906+Mephisto5558@users.noreply.github.com>
2026-03-05 11:55:58 +0100

    use errorHandler middleware instead of try/catch and improve error logging

--- src/backend/middleware/notFound.ts
+++ src/backend/middleware/notFound.ts
@@ -1,6 +1,11 @@
 import { constants } from 'node:http2';
 import type { RequestHandler } from 'express';
+import type { ErrorRes } from '@/shared/types/api.js';
 
-export const notFound: RequestHandler = (_req, res) => {
-  res.sendStatus(constants.HTTP_STATUS_NOT_FOUND);
+export const notFound: RequestHandler<unknown, ErrorRes> = (req, res) => {
+  res
+    .status(constants.HTTP_STATUS_NOT_FOUND)
+    .send({
+      status: 'error', error: `Either this route does not exist or it does not handle "${req.method}" requests.`
+    });
 };
--- src/backend/routes/api/v1/items/index.ts
+++ src/backend/routes/api/v1/items/index.ts
@@ -1,45 +1,27 @@
-import { constants } from 'node:http2';
 import { Router } from 'express';
 import { db } from '@/backend/middleware/auth.js';
 import type { Request } from 'express';
 
 import type { getItemErrRes, getItemSuccessRes, getItemsErrRes, getItemsSuccessRes } from '@/shared/types/api.js';
 import type { Item } from '@/shared/types/db.js';
-import type {
-  getItemErrRes, getItemSuccessRes, getItemsErrRes, getItemsSuccessRes
-} from '@/shared/types/globals.js';
-
-const errRes = (err: string): getItemsErrRes | getItemErrRes => ({ status: 'error', error: err });
 
 export default Router()
   .get('/{:q}', async (
     req: Request<unknown, getItemsErrRes | getItemsSuccessRes, unknown, { q?: string }>,
     res
   ) => {
-    try {
-      const queryBuilder = db<Item>('item').select('*');
+    const queryBuilder = db<Item>('item');
 
-      if (req.query.q) queryBuilder.whereLike('description', `%${req.query.q}%`);
-      const items = await queryBuilder;
+    if (req.query.q) queryBuilder.whereLike('description', `%${req.query.q}%`);
+    const items = await queryBuilder;
 
-      return res.json({ status: 'success', items });
-    }
-    catch (err) {
-      console.error(err);
-      return res.status(constants.HTTP_STATUS_INTERNAL_SERVER_ERROR).json(errRes('Internal Server Error'));
-    }
+    return res.json({ status: 'success', items });
   })
   .get('/{item}', async (
     req: Request<{ item: string }, getItemErrRes | getItemSuccessRes, unknown>, res, next
   ) => {
-    try {
-      const item = await db<Item>('item').where('id', '=', req.params.item).first();
-      if (!item) return next(); // 404 handler
+    const item = await db<Item>('item').where('id', req.params.item).first();
+    if (!item) return next(); // 404 handler
 
-      return res.json({ status: 'success', item });
-    }
-    catch (err) {
-      console.error(err);
-      return res.status(constants.HTTP_STATUS_INTERNAL_SERVER_ERROR).json(errRes('Internal Server Error'));
-    }
+    return res.json({ status: 'success', item });
   });
